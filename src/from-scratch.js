12672b42e6dcc2cb6e55c33c17e6f60a27c6175f
embryobirthday <alexinverda@gmail.com>
2024-04-11 16:28:16 -0400

    all odds done :]

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,42 +1,73 @@
-const coolGreeting = () => {
-};
-
-const haveBirthday = () => {
-};
-
-const becomeSecretAgent = () => {
-};
-
-const carMaker = () => {
-};
-
-const weAreNotFriends = () => {
-};
-
-const listHobbies = () => {
-};
-
-const getNextOpponent = () => {
-};
-
-const listAllKeys = () => {
-};
-
-const listAllValues = () => {
-};
-
-const convertToMatrix = () => {
-};
-
-module.exports = {
-  coolGreeting,
-  haveBirthday,
-  becomeSecretAgent,
-  carMaker,
-  weAreNotFriends,
-  listHobbies,
-  getNextOpponent,
-  listAllKeys,
-  listAllValues,
-  convertToMatrix,
-};
+//1.
+const coolGreeting = (person) => {
+  if (person.isCool === true){
+    return `What is UP ${person.name.toUpperCase()}? How you been doin'?`
+  } else {
+    return `Greetings ${person.name}, how have you been lately?`
+  }
+  };
+  
+  
+  //2.
+  const haveBirthday = () => {
+  };
+  
+  
+  //3.
+  const becomeSecretAgent = (person, spyHandle) => {
+    delete person.name
+    person.spyHandle = spyHandle //to add properties, you state the object name followed by dot notation with the name of the new key, then assign the key a value. 
+  };
+  
+  
+  
+  //4. 
+  const carMaker = () => {
+  };
+  
+  //5.
+  const weAreNotFriends = (person) => {
+  return person.friends.pop() //just had to access the friends array and use the .pop() method on it
+  };
+  
+  
+  //6.
+  const listHobbies = () => {
+  };
+  
+  //7.
+  const getNextOpponent = (team) => {
+    if(team.matches[0] === undefined) {
+      return null 
+    }
+      let nextTeam = team.matches[0]; //make a variable for the opposing teams 
+      let oppTeamName = nextTeam.teamName
+      return oppTeamName //since you can use notation on the variable instead of index notation ([0]), it fetches the team!
+  }
+  
+  //8.
+  const listAllKeys = () => {
+  };
+  
+  //9. 
+  const listAllValues = (obj) => {
+    return Object.values(obj)
+  };
+  
+  //.10
+  const convertToMatrix = () => {
+  };
+  
+  module.exports = {
+    coolGreeting,
+    haveBirthday,
+    becomeSecretAgent,
+    carMaker,
+    weAreNotFriends,
+    listHobbies,
+    getNextOpponent,
+    listAllKeys,
+    listAllValues,
+    convertToMatrix,
+  };
+  
---
+++ src/playground.js
@@ -0,0 +1,4 @@
+const haveBirthday = (person) => {
+    person.age += 1 //+= adding to the number, and reassigning what you're adding to 
+   };
+   
