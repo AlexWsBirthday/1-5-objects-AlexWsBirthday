f6009c943ed2f13351f5024b86b6605633ee5b4f
embryobirthday <alexinverda@gmail.com>
2024-04-11 20:32:16 -0400

    short answers all done :]

--- src/playground.js
+++ src/playground.js
@@ -2,3 +2,10 @@ const haveBirthday = (person) => {
     person.age += 1 //+= adding to the number, and reassigning what you're adding to 
    };
    
+
+//    const {name, age} = user;
+//    user.name = "sid"
+//    user.age = 17
+
+//    console.log(user)
+
